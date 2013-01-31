c67135794f078d6d534dda2d622a70a675faf02b
stolinski <scotttolinski@gmail.com>
2013-01-31 00:20:28 -0500

    Adds Bootstrap, Types Model, and Devise Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Sets::Application.routes.draw do
+  resources :types
+
+
   devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  root :to => "devise/registrations#new"
+  root :to => "types#index"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
