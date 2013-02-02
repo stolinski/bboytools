96f85a9a8954c3fcfae26e3145cfbbfb0a2407ad
stolinski <scotttolinski@gmail.com>
2013-02-01 21:43:25 -0500

    Adds Tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :bboy_name, :crew_name
   # attr_accessible :title, :body
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bboy_name, presence: true
+  validates :email, presence: true,  uniqueness: true
+
   has_many :moves
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test 'a user should enter a first name' do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end
+
+	test 'a user should enter a last name' do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+
+	test 'a user should enter a bboy name' do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:bboy_name].empty?
+	end
+
+	test 'a user should have an email' do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:email].empty?
+	end
+
+	test 'a user should have a unique email' do
+		user = User.new
+
+		user.email = users(:scott).email
+		assert !user.save
+		assert !user.errors[:email].empty?
+	end
+
 end
